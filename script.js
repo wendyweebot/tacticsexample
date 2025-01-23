2480ef78bc271eee17f14a76fc5df4ba23196a91
wendy <92373286+wendyweebot@users.noreply.github.com>
2025-01-23 16:06:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let counter ;
 
 const incrementBy1 = async () => {
   // replace this with your own fetch by clicking "manage" after hovering over the incrementBy1 tactic in your workspace
-  const response = await fetch("https://api.tactics.dev/api/workspace/e7dbec80-ac3d-4453-9b91-ba92e7ca52d9/run", {
+  const response = await fetch("https://api.tactics.dev/api/workspace/76145cab-6a6a-413f-967b-6c4d05c65a8a/run", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ initial_variables: {}, tactic_id: "b08cbcad-5331-4708-baa4-a8ad2f834d4a" }) ,
+    body: JSON.stringify({ initial_variables: {}, tactic_id: "171f815b-58e1-4e21-a3ce-5f962e6ed2b7" }) ,
   }) ;
   const result = await response.json() ;
   counter = result.result.content.value ;
@@ -14,10 +14,10 @@ const incrementBy1 = async () => {
 
 const incrementBy2 = async () => {
   // replace this with your own fetch by clicking "manage" after hovering over the incrementBy2 tactic in your workspace
-  const response = await fetch("https://api.tactics.dev/api/workspace/e7dbec80-ac3d-4453-9b91-ba92e7ca52d9/run", {
+  const response = await fetch("https://api.tactics.dev/api/workspace/76145cab-6a6a-413f-967b-6c4d05c65a8a/run", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ initial_variables: {}, tactic_id: "7666d0c0-983f-4356-8818-38b397e98b6a" }) , 
+    body: JSON.stringify({ initial_variables: {}, tactic_id: "5f15fce4-3949-4c84-873d-1967f6444da8" }) , 
   }) ;
   const result = await response.json() ;
   counter = result.result.content.value ;
@@ -26,10 +26,10 @@ const incrementBy2 = async () => {
 
 const fetchCounter = async () => {
   // replace this with your own fetch by clicking "manage" after hovering over the fetchCounter tactic in your workspace
-  const response = await fetch("https://api.tactics.dev/api/workspace/e7dbec80-ac3d-4453-9b91-ba92e7ca52d9/run", {
+  const response = await fetch("https://api.tactics.dev/api/workspace/76145cab-6a6a-413f-967b-6c4d05c65a8a/run", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ initial_variables: {}, tactic_id: "5b69b6ed-cc60-4c06-8b01-dcc47ec5eaf4" }) ,
+    body: JSON.stringify({ initial_variables: {}, tactic_id: "fef9c943-fa66-4e46-b22a-46031519a26c" }) ,
   }) ;
   const result = await response.json() ;
   counter = result.result.content.value ;
